deac1e27bb3d3b41d983d249b6a7ba7f9c697ecb
Renyi Tan <tanrenyi@gmail.com>
2020-05-05 16:01:55 +0800

    frame can read mouse position

--- main.py
+++ main.py
@@ -2,53 +2,85 @@ import pygame
 import sys
 
 # game settings
-WIDTH = 1024
-HEIGHT = 768
-SCREEN_SIZE = (WIDTH, HEIGHT)
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 800
 FPS = 60
 TITLE = 'GRID'
-BGCOLOR = (255, 255, 255)  # dark grey
 
-TILE_SIZE = 32
-GRIDWIDTH = (int)(WIDTH / TILE_SIZE)
-GRIDHEIGHT = (int)(HEIGHT / TILE_SIZE)
+TILE_SIZE = 16
+NUM_COLS = (int)(SCREEN_WIDTH / TILE_SIZE)
+NUM_ROWS = (int)(SCREEN_HEIGHT / TILE_SIZE)
+
+# color constants
+COLOR_RED = (255, 0, 0)
+COLOR_BLUE = (0, 255, 0)
+COLOR_BLUE = (0, 0, 255)
+COLOR_WHITE = (255, 255, 255)
+COLOR_BLACK = (0, 0, 0)
+COLOR_GREY = (211, 211, 211)
+
+# initialisation
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen.fill(COLOR_WHITE)
 
 
-class Node:
-    def __init__(self, x, y, surface):
-        self.x = x
-        self.y = y
-        self.size = TILE_SIZE
-        self.rect = (self.x, self.y, self.size, self.size)
-        self.color = (0, 0, 0)
-        self.width = 1
-        self.screen = surface
+class Tile:
+    def __init__(self, row, col):
+        self.x = col
+        self.y = row
+        self.is_obstacle = False  #
+        self.color = COLOR_BLACK  # default color
+        self.t_type = 1  # default, no fill
 
-    def draw(self):
-        pygame.draw.rect(self.screen, self.color, self.rect, self.width)
+    def show(self):
+        if self.is_obstacle:
+            self.color = COLOR_GREY
+            self.t_type = 0
 
+        pygame.draw.rect(screen, self.color, (self.x * TILE_SIZE,
+                                              self.y * TILE_SIZE, TILE_SIZE, TILE_SIZE), self.t_type)
+
+
+# Create grid 2d array; grid[row][col]
+grid = [[0 for i in range(NUM_ROWS)] for j in range(NUM_COLS)]
+# fill grid with empty Tile
+for i in range(NUM_ROWS):
+    for j in range(NUM_COLS):
+        grid[i][j] = Tile(i, j)
+
+# set screen borders as obstacles
+for i in range(NUM_ROWS):
+    for j in range(NUM_COLS):
+        grid[0][j].is_obstacle = True
+        grid[NUM_ROWS-1][j].is_obstacle = True
+    grid[i][0].is_obstacle = True
+    grid[i][NUM_COLS-1].is_obstacle = True
+
+
+def show_grid():
+    for i in range(NUM_ROWS):
+        for j in range(NUM_COLS):
+            grid[i][j].show()
 
 def main():
     pygame.init()
-    screen = pygame.display.set_mode(SCREEN_SIZE)
-    screen.fill(BGCOLOR)
-
-    for y in range(GRIDHEIGHT):
-        for x in range(GRIDWIDTH):
-            n = Node(x * 32, y * 32, screen)
-            n.draw()
 
     while (True):
-        for event in pygame.event.get():
-            if (event.type) == pygame.QUIT:
+
+        # display grid
+        show_grid()
+
+        events = pygame.event.get()
+
+        for ev in events:
+            if (ev.type) == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
-            if (event.type == pygame.MOUSEBUTTONDOWN):
-                print('it\'s down')
-
-            if (event.type == pygame.MOUSEBUTTONUP):
-                print('it\'s up')
+            # checks for left mouse down. (left, scroll, right): boolean
+            if pygame.mouse.get_pressed()[0]:
+                position = pygame.mouse.get_pos()
+                print(position)
 
         pygame.display.update()
 
